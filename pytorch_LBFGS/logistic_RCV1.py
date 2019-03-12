cfc0c3db82ef0d91c0d111f57f49085ceb9ef350
chenhongluo <2550795728@qq.com>
2019-03-12 17:11:52 +0800

    update logstic_RCV1

--- pytorch_LBFGS/logistic_RCV1.py
+++ pytorch_LBFGS/logistic_RCV1.py
@@ -136,9 +136,10 @@ Nk_size = int((1 - 2 * overlap_ratio) * batch_size)
 end = 0
 begin = time.time()
 random_index = np.random.permutation(range(len(train_dataset)))
-end1 = time.time() - begin
+
 Ok_prev = random_index[0:Ok_size]
 X_trains,y_trains = train_dataset.getItems(Ok_prev)
+end1 = time.time() - begin
 g_Ok_prev, obj_Ok_prev = get_grad(optimizer, X_trains, y_trains, opfun)
 end = time.time() - begin
 print(end1,end)
@@ -187,15 +188,15 @@ for n_iter in range(max_iter):
 
 
     model.eval()
-    train_loss, test_loss, test_acc = compute_stats(X_trains, y_trains, np.array([]),
-                                                    np.array([]), opfun, accfun, ghost_batch=128)
+    # train_loss, test_loss, test_acc = compute_stats(X_trains, y_trains, np.array([]),
+    #                                                 np.array([]), opfun, accfun, ghost_batch=128)
 
     all_test_loss = 0.0
     all_test_acc = 0.0
     count = (80-30)/10
     for i in range(int(count)):
         X_tests, y_tests = test_dataset.getItems(range(0,i*10000))
-        train_loss, test_loss, test_acc = compute_stats(np.array([]), np.array([]), X_tests, y_tests, opfun, accfun, ghost_batch=128)
+        train_loss, test_loss, test_acc = compute_stats(X_trains, y_trains, X_tests, y_tests, opfun, accfun, ghost_batch=128)
         all_test_acc += test_acc
         all_test_loss += test_loss
     all_test_loss/=10.0
